6c2eb3bfc24d0e5edbfb0bbf9d18b06d2c64fd97
MIHAEL ANTOLOVIC <antolovicmihael@gmail.com>
2020-05-14 20:27:06 +0200

    Zadatak 1

---
+++ Lv4/Lv4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lv4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dataset data = new Dataset("C:\\Users\\MIHAEL\\Documents\\LV4-Doop\\DoopFIle.csv");
+            IAnalytics analyze = new Adapter(new Analyzer3rdParty());
+            double[] colmn = analyze.CalculateAveragePerColumn(data);
+            double[] row = analyze.CalculateAveragePerRow(data);
+        }
+    }
+}
